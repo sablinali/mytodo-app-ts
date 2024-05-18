014cf9918c68c45dfa27c3ee10fdbc457908f992
sablinali <sablinahare@gmail.com>
2024-05-18 19:00:21 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { TodoTitle } from "../types";
+import { CreateTodo } from "./CreateTodo";
+import TSlogo from "../assets/ts_logo.jpeg";
+
+interface Props {
+  onAddTodo: ({ title }: TodoTitle) => void;
+}
+
+export const Header: React.FC<Props> = ({ onAddTodo }) => {
+  return (
+    <>
+      <header className="header" />
+      <h1>
+        Todo
+        <img style={{ width: "60px", height: "auto" }} src={TSlogo} />
+      </h1>
+      <CreateTodo saveTodo={onAddTodo} />
+    </>
+  );
+};
---
+++ src/components/Todo.tsx
@@ -0,0 +1,41 @@
+import { type Todo as TodoType, type TodoId } from "../types";
+
+interface Props extends TodoType {
+  onToggleCompleteTodo: ({
+    id,
+    completed,
+  }: Pick<TodoType, "id" | "completed">) => void;
+  onRemoveTodo: ({ id }: TodoId) => void;
+}
+
+export const Todo: React.FC<Props> = ({
+  id,
+  title,
+  completed,
+  onRemoveTodo,
+  onToggleCompleteTodo,
+}) => {
+  const handleChangeCheckbox = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    onToggleCompleteTodo({
+      id,
+      completed: event.target.checked,
+    });
+  };
+
+  return (
+    <div className="view">
+      <input
+        className="toggle"
+        checked={completed}
+        type="checkbox"
+        onChange={(event) => {
+          onToggleCompleteTodo({ id, completed: event.target.checked });
+        }}
+      />
+      <label>{title}</label>
+      <button className="destroy" onClick={() => onRemoveTodo({ id })} />
+    </div>
+  );
+};
